72c15a4b17e1662059b690fbbe7045145004fba1
Ryan Kois <ryan.kois@gmail.com>
2014-04-20 23:12:28 -0700

    Update gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 var gulp = require('gulp')
 var uglify = require('gulp-uglify')
 var header = require('gulp-header')
+var transform = require('vinyl-transform')
+var map = require('map-stream')
 
 gulp.task('thingy', function() {
+  var encoder = transform(function(filename) {
+    return map(function(chunk, next) {
+      return next(null, encodeURIComponent(chunk.toString()))
+    })
+  })
   gulp.src('index.js')
     .pipe(uglify())
+    .pipe(encoder)
     .pipe(header('javascript:'))
     .pipe(gulp.dest('dist'))
 });
